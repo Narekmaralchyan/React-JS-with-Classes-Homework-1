093a072963a2d2437b6c000a1eac8cddd2532bcd
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-09-26 22:46:33 +0400

    first commit

---
+++ src/Components/counter/index.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Counter extends Component{
+
+    render(){
+        return(
+            <div className="Counter">
+                0
+            </div>
+        )
+    }
+}
+
+export default Counter;
